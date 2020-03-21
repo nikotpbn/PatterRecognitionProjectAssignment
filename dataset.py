78f2d1352f218150042647805b43329f0cdc0bb4
Nicolau Neto <nikobeltrao@gmail.com>
2020-03-21 08:03:47 +0000

    dataset.py now transforming data from string to float + change on the line substring to gather information in the corret way

--- dataset.py
+++ dataset.py
@@ -1,10 +1,10 @@
 import glob
 import pathlib
-import json
 
+# Variable to help debug code
 debug = False
 
-# Path to the project file
+# Path to the project folder
 current_dir = pathlib.Path().absolute()
 
 # Paths to accelerometer and gyroscope dataset's
@@ -26,61 +26,58 @@ dataset = {
 }
 
 
-# Function to import values for phone accelerometer dataset
-def add_data(device, input_type, data):
-    file_count = 0
+# Function to import data from txt files
+def add_data(device, sensor, data):
 
     for f in data:
         # Open file and get the 1st line
         file = open(f)
-        file_count += 1
         line = file.readline().split(",")
 
         # Get subject id from line
         subject_id = line[0]
 
         # Add subject to the dataset
-        dataset[device][input_type][subject_id] = {}
-        print(file_count, ": New subject created ->", subject_id) if debug is True else False
+        dataset[device][sensor][subject_id] = {}
+        # print(file_count, ": New subject created ->", subject_id) if debug is True else False
         sample = 0
 
-        # Add samples of the subject to the dataset
+        # Add subject sample values to the dataset
         for line in file:
             # Split the data
             data_str = line.split(",")
 
             # Remove the last part of markup on the line --(";\n")--
-            data_str[5] = data_str[5][0:len(data_str[5]) - 3]
+            data_str[5] = data_str[5][0:len(data_str[5]) - 2]
 
-            # Gather subject data
-            data = {
+            # Try to parse data from string to float
+            try:
+                x = float(data_str[3])
+                y = float(data_str[4])
+                z = float(data_str[5])
 
-                "activity_code": data_str[1],
-                "timestamp": data_str[2],
-                "x_axis": data_str[3],
-                "y_axis": data_str[4],
-                "z_axis": data_str[5]
-            }
+                # Assign subject data
+                data = {
+                    "activity_code": data_str[1],
+                    "timestamp": data_str[2],
+                    "x_axis": x,
+                    "y_axis": y,
+                    "z_axis": z
+                }
 
-            # Add to dictionary
-            dataset[device][input_type][subject_id][sample] = data
-            sample += 1
-        print("Finished adding samples for: ", subject_id) if debug is True else False
+                # Add to dataset dictionary
+                dataset[device][sensor][subject_id][sample] = data
+                sample += 1
 
+            except ValueError:
+                print("Could not parse string to float at sample ", sample, "for subject ", subject_id, "Values: x->",
+                      data_str[3], ", y->", data_str[4], ", z", data_str[5])
 
-def write_json_file(filename):
-    json_dataset = json.dumps(dataset)
-    try:
-        with open(filename, "w") as f:
-            f.writelines(json_dataset)
-
-        print(filename + " has been created.")
-    except Exception as e:
-        print(str(e))
+        print("Finished adding ", sample, "samples for: ", subject_id) if debug is True else False
 
 
 # Function to print phone accelerometer data values
-# TODO: Functions to print other values(?)
+# TODO: Change function to print generalized values is necessary(?)
 def print_phone_accel_dataset():
     for subject in dataset["phone"]["accel"]:
         for sample in dataset["phone"]["accel"][subject]:
@@ -96,20 +93,17 @@ class Dataset:
         phone_gyro = glob.glob(phone_gyro_paths)
 
         print("Starting to add data...might take a few minutes!")
-        # Add data to the set structure
-        print("Adding phone accelerometer data...")
+        # Read all dataset's
+        print("Reading phone accelerometer data...")
         add_data("phone", "accel", phone_accel)
 
-        print("Adding watch accelerometer data...")
-        #add_data("watch", "accel", watch_accel)
+        print("Reading watch accelerometer data...")
+        add_data("watch", "accel", watch_accel)
 
-        print("Adding watch gyroscope data...")
-        #add_data("watch", "gyro", watch_gyro)
+        print("Reading watch gyroscope data...")
+        add_data("watch", "gyro", watch_gyro)
 
-        print("Adding phone gyroscope data...")
-        #add_data("phone", "gyro", phone_gyro)
+        print("Reading phone gyroscope data...")
+        add_data("phone", "gyro", phone_gyro)
 
         print("Finished!")
-
-        print("Writing the json file...might take a while!")
-        write_json_file("dataset.json")
