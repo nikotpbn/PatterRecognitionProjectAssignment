f8e6615a6796dd8cfa640ed8b994e5b1f82c5ef7
Paulo Viegas <pauloviegas93@gmail.com>
2020-04-01 16:55:03 +0100

    Feature Selection

--- arff_dataset.py
+++ arff_dataset.py
@@ -4,13 +4,13 @@ import re
 import numpy
 
 # Dúvidas:
-# 1) Qual dataset usar?
+# 1) Qual dataset usar? so arff
 # 2) assignment da o exemplo de dividir os dataset em 4 partes (cell e smart: acel, giro), tem q ser assim? dado o
-# objetivo o melhor n seria por atividades?
+# objetivo o melhor n seria por atividades? na tela escolher apenas uma dos datasets
 # 3) No final provavelmente vamos excluir as msms features do artigo que vem no datset so q temos q justificar com as
-# tecnicas correto?
+# tecnicas correto? correto
 # 4) Optamos por fazer com o python e vamos utilizar a biblioteca indicada (scikit-learn), ela cobre todas as areas
-# necessarias? Teremos que usar algumas outra para complementar?
+# necessarias? Teremos que usar algumas outra para complementar? pandas...
 
 
 class Dataset:
@@ -20,64 +20,27 @@ class Dataset:
             "accel": {
                 "data": {},
                 "target": {},
-                "title": {}
+                "label": {}
             },
             "gyro": {
                 "data": {},
                 "target": {},
-                "title": {}
+                "label": {}
             }
         },
         "watch": {
             "accel": {
                 "data": {},
                 "target": {},
-                "title": {}
+                "label": {}
             },
             "gyro": {
                 "data": {},
                 "target": {},
-                "title": {}
+                "label": {}
             }
         }
     }
-
-    def add_data(self, data_files, device, sensor):
-        # Variable
-        file_number = 1
-        data = []
-        target = []
-        title = []
-
-        # Read each data in vector data_files
-        for data_file in data_files:
-            file = open(data_file)
-            # Variable to control the lines in the file, to separate the sample to the titles
-            line = 1
-            # Read each line in each file
-            for sample in file:
-                if 4 <= line <= 94 and file_number == 1:
-                    # Search for titles of each attribute
-                    found = re.search('"(.+?)"', sample).group(1)
-                    title.append(found)
-                elif line > 97 and sample is not None:
-                    # Work in each sample in the file
-                    read = sample.split(",")
-                    # Add the first value in the sample (activity) in the vector target
-                    target.append(read[0])
-                    # Eliminate the target (activity - first value) and the subject (individual - last value) from the
-                    # sample
-                    read.pop(0)
-                    read.pop(-1)
-                    # Add the vector read in the vector data
-                    data.append(read)
-                line += 1
-            file_number += 1
-
-        # Add the data read into the dataset attribute
-        self.dataset[device][sensor]["data"] = numpy.asarray(data)
-        self.dataset[device][sensor]["target"] = numpy.asarray(target)
-        self.dataset[device][sensor]["title"] = numpy.asarray(title)
 
     def __init__(self):
         # Info
@@ -114,3 +77,50 @@ class Dataset:
         # Watch-gyroscope data
         print("Reading watch-gyroscope...")
         self.add_data(watch_gyro, "watch", "gyro")
+
+    def add_data(self, data_files, device, sensor):
+        # Variable
+        file_number = 1
+        data = []
+        target = []
+        label = []
+
+        # Read each data in vector data_files
+        for data_file in data_files:
+            file = open(data_file)
+            # Variable to control the lines in the file, to separate the sample to the label
+            line = 1
+            # Read each line in each file
+            for sample in file:
+                if 4 <= line <= 94 and file_number == 1:
+                    # Search for labels of each attribute
+                    found = re.search('"(.+?)"', sample).group(1)
+                    label.append(found)
+                elif line > 97 and sample is not None:
+                    # Work in each sample in the file
+                    read = sample.split(",")
+                    # Add the first value in the sample (activity) in the vector target
+                    target.append(read[0])
+                    # Eliminate the target (activity - first value) and the subject (individual - last value) from the
+                    # sample
+                    read.pop(0)
+                    read.pop(-1)
+                    # Add the vector read in the vector data
+                    data.append(read)
+                line += 1
+            file_number += 1
+
+        # Add the data read into the dataset attribute
+        self.dataset[device][sensor]["data"] = numpy.asarray(data).astype(numpy.float64)
+        self.dataset[device][sensor]["target"] = numpy.asarray(target)
+        self.dataset[device][sensor]["label"] = numpy.asarray(label)
+
+    def get_database(self, device_sensor):
+        if device_sensor == "PA":
+            return self.dataset["phone"]["accel"]
+        elif device_sensor == "PG":
+            return self.dataset["phone"]["gyro"]
+        elif device_sensor == "WA":
+            return self.dataset["watch"]["gyro"]
+        elif device_sensor == "WG":
+            return self.dataset["watch"]["gyro"]
--- test.py
+++ test.py
@@ -1,5 +1,26 @@
 # Imports
 from arff_dataset import Dataset
+import pandas as pd
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
 
-# Code to execute the test
-x = Dataset()
+# Load the dataset
+data = Dataset().get_database("PA")
+
+# -------------------- Feature selection (K bests) --------------------
+# Choosing the K best features
+selector = SelectKBest(f_classif, k=5)
+new_data = selector.fit_transform(data["data"], data["target"])
+indexes = selector.get_support(indices=True)
+new_label = [data["label"][i] for i in indexes]
+
+# Update dataset replacing the old data and label to the bests
+data["data"] = new_data
+data["label"] = new_label
+
+# TODO -------------------- Feature Reduction --------------------
+
+# TODO -------------------- Classifiers --------------------
+
+# -------------------- Print Results --------------------
+print(data)
